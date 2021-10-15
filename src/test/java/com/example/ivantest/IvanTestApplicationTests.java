ac8c4270855476b3d3c5496a2ec2e6632362892f
Ivan Khurudzhi <khurudzhi@wisc.edu>
2021-10-15 13:03:54 -0500

    Test

---
+++ src/test/java/com/example/ivantest/IvanTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ivantest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IvanTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
